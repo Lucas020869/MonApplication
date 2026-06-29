2a8d07e9fed87706d38c5711aba276b805c18dc4
Lucas020869 <uriot.lucas@gmail.com>
2026-06-29 20:27:26 +0200

    correction émulateur

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.monapplicationjeux"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.monapplicationjeux"
